073b384c998fbb07aa9b0eb6235fe37553d91dde
Bloodline-2000 <1065673488@qq.com>
2021-08-09 21:50:40 +0800

    graph search done

---
+++ graph_search.py
@@ -0,0 +1,90 @@
+from tqdm import tqdm
+from py2neo import Graph, Node, Relationship
+
+def associate_author(graph, name, org):
+    instruction = '''match (:author{name:"%s", org:"%s"})-[*2..2]-(p:author)
+return  p.name, p.org ''' % (name, org)
+    result = graph.run(instruction).data()
+    res = []
+    for i in result:
+        res.append((i["p.name"],i["p.org"]))
+    res = list(set(res))
+    return res
+
+def associate_paper(graph, title):
+    instruction = '''match (:paper{title:"%s"})-[*1..1]-(p:paper)
+return  p.title ''' % (title)
+    result = graph.run(instruction).data()
+    res = []
+    for i in result:
+        res.append((i["p.title"]))
+    res = list(set(res))
+    return res
+
+def shortest_path(graph, name1, org1, name2, org2):
+    instruction= '''match (a:author{name:"%s", org:"%s"}), (b:author{name:"%s", org:"%s"}),
+p = shortestPath((a)-[*..15]-(b))
+return p ''' %(name1, org1, name2, org2)
+    result = graph.run(instruction).data()
+    return result
+
+def associate_paper(graph, title):
+    instruction = '''match (:paper{title:"%s"})-[*1..1]-(p:paper)
+return  p.title ''' % (title)
+    result = graph.run(instruction).data()
+    res = []
+    for i in result:
+        res.append((i["p.title"]))
+    res = list(set(res))
+    return res
+    
+def find_authors(graph, title):
+    instruction = '''match ((p:author)-[*1..1]-(b:paper{title:"%s"}))
+return  p.name, p.org ''' % (title)
+    result = graph.run(instruction).data()
+    res = []
+    for i in result:
+        res.append((i["p.name"],i["p.org"]))
+    res = list(set(res))
+    return res    
+
+def common_paper(graph, name1, org1, name2, org2):
+    instruction = '''match ((:author{name:"%s", org:"%s"})-[*1..1]-(b:paper))
+return  b.title ''' % (name1, org1)
+    result1 = graph.run(instruction).data()
+    res1 = [i["b.title"] for i in result1]
+    instruction = '''match ((:author{name:"%s", org:"%s"})-[*1..1]-(b:paper))
+return  b.title ''' % (name2, org2)
+    result1 = graph.run(instruction).data()
+    res2 = [i["b.title"] for i in result1]
+    res = list(set(res1).intersection(set(res2)))
+    return res
+
+def pagerank(graph, title):
+    instruction = '''match (p:paper{title:"%s"})
+return  p.pagerank ''' % (title)
+    result = graph.run(instruction).data()
+    return result[0]["p.pagerank"]
+
+if __name__ == '__main__':
+    graph = Graph('bolt://40.114.125.234:7687',username='neo4j',password='123456aa')
+    # Find Related Author
+    Author_name, Author_org = "Nestor Garay-Vitoria", "-1"
+    print(associate_author(graph, Author_name, Author_org))
+    # Find Relate Paper
+    Paper_title = "Rothko's Negative Theology"
+    print(associate_paper(graph, Paper_title))
+    # Find the shortest path between two authors
+    author1_name, author1_org = "Nestor Garay-Vitoria", "-1"
+    author2_name, author2_org = "Julio Abascal", "-1"
+    print(shortest_path(graph, author1_name, author1_org, author2_name, author2_org))
+    # Coauthorship:
+    # Find all authors of a paper
+    Paper_title = "Evaluation of Prediction Methods Applied to an Inflected Language."
+    print(find_authors(graph, Paper_title))
+    # Find papers that two authors coauthor on 
+    author1_name, author1_org = "Nestor Garay-Vitoria", "-1"
+    author2_name, author2_org = "Julio Abascal", "-1"
+    print(common_paper(graph, author1_name, author1_org, author2_name, author2_org))
+    Paper_title = "Action of diethylstilboestrol on mouse vaginal sialic acids. I."
+    print(pagerank(graph, Paper_title))
